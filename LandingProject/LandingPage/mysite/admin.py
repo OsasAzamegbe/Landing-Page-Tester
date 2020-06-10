dc4db11f8cf795e6233dd074095a33ad827606ce
MdBilal420 <mohdbilal16@gmail.com>
2020-06-11 00:35:39 +0530

    chore: Landing Signup Page

---
+++ LandingProject/LandingPage/mysite/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from mysite.models import Info
+
+admin.site.register(Info)
---
+++ LandingProject/LandingPage/mysite/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import Info
+
+
+def index(request):
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        uname = request.POST.get('uname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        s = Info(firstname = fname,lastname = lname,username = uname, email = email,password = password,)
+        s.save()
+        c = Info.objects.filter(username='uname').count()
+        print(c)
+    return render(request, 'mysite/index.html')
+
+
