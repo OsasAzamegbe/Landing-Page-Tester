6dc9dbd9d0de6b897314802e44ad3bc240023fa4
Kayode <oluwaseyikayode25@gmail.com>
2020-06-11 09:12:02 +0100

    added swagger docs

--- PageTester/urls.py
+++ PageTester/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from landingPageTester import views as LPT_views
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Landing Page Tester',
+        default_version='v1',
+        description='Test',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='contact@snippets.local'),
+        license=openapi.License(name='BSD License')
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +43,6 @@ urlpatterns = [
     path('delete_page/<int:pk>/', LPT_views.delete_page, name='delete_page'),
     path('manage/<int:pk>/', LPT_views.manage, name='manage'),
 	path('edit_url/', LPT_views.edit_url, name='edit_url'),
-	path('get_url/<int:pk>/', LPT_views.get_url, name='get_url')
+	path('get_url/<int:pk>/', LPT_views.get_url, name='get_url'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
