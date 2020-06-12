919e6a243a919397d5b3931a16ca302ba14be5f4
Michael Ajanaku <remiljw@gmail.com>
2020-06-12 12:23:16 +0100

    modified settings

--- PageTester/settings.py
+++ PageTester/settings.py
@@ -119,7 +119,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 
