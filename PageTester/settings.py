b66c7224ebf24088c0445c94b289003b5af1f71e
Kang Breder Mbulle <kangbreder@gmail.com>
2020-06-12 16:00:04 +0100

    updated docker

--- PageTester/settings.py
+++ PageTester/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'PageTester.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+        "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
