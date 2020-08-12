80cec5c77227f73a4633bfeade9a2b47ff2416c3
Michael Ajanaku <remiljw@gmail.com>
2020-08-12 16:05:00 +0100

    url api fix

--- PageTester/urls.py
+++ PageTester/urls.py
@@ -15,51 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework.documentation import include_docs_urls
 from landingPageTester import views as LPT_views
 from django.urls import path, re_path
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Landing Page Tester',
-        default_version='v1',
-        description='Microservice for Testing Landing Pages',
-        terms_of_service='https://www.google.com/policies/terms/',
-        contact=openapi.Contact(email='contact@snippets.local'),
-        license=openapi.License(name='BSD License')
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,)
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('v1/', include('landingPageTester.urls')),
-    # path('', LPT_views.index, name='index'),
-    # path('add_page/', LPT_views.add_page, name='add_page'),
-    # path('get_status/', LPT_views.get_status, name='get_status'),
+	# path('v1/', include('landingPageTester.urls')),
+    path('', LPT_views.index, name='index'),
+    path('add_page/', LPT_views.add_page, name='add_page'),
+    path('get_status/', LPT_views.get_status, name='get_status'),
     # path('landingpagetester/', LPT_views.TestPage, name='testPage'),
-    # path('delete_page/<int:pk>/', LPT_views.delete_page, name='delete_page'),
-    # path('manage/<int:pk>/', LPT_views.manage, name='manage'),
-	# path('edit_url/', LPT_views.edit_url, name='edit_url'),
-	# path('get_url/<int:pk>/', LPT_views.get_url, name='get_url'),
-    # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^v1/documentation(?P<format>\.json)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', include_docs_urls(title='Landing Page Tester', description=
-    """Microservice for Testing Various Landing Pages.Testing is simple,
-    register below in the /v1/register/ or using httpie using the command http post then go to /api-token-auth below to get your token or alternatively
-    via the Command line  '/api-token-auth/ username=   password= ' using httpie via the command http post.Testing can be done here 
-    or via a testing tool like Postman or via the Command Line using httpie.
-    If testing via Postman add your Token to the header Key = Authorization,Value = Token <your token>.
-    If testing using httpie always add 'Authorization: Token <your token>' after each end point.
-    To test here go to Authentication in the side bar ,click on token. Fill in the Bearer field with 'Token' then the API key field with your generated token.
-    You are good to go.
-                                   
-"""
-    
-    ,permission_classes=(permissions.AllowAny,)))
+    path('delete_page/<int:pk>/', LPT_views.delete_page, name='delete_page'),
+    path('manage/<int:pk>/', LPT_views.manage, name='manage'),
+	path('edit_url/', LPT_views.edit_url, name='edit_url'),
+	path('get_url/<int:pk>/', LPT_views.get_url, name='get_url')
 ]
                                
--- landingPageTester/urls.py
+++ landingPageTester/urls.py
@@ -1,23 +1,14 @@
 from django.urls import path,include
 from django.conf.urls import url
-from .apiviews import * 
-from rest_framework.authtoken.views import obtain_auth_token
+
+
 
 # from rest_framework_simplejwt import views as jwt_views
 
 app_name = 'landingPageTester'
 
 urlpatterns = [
-	# path('token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain'),
-	# path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token-refresh'),
-    path('traffic/<path:url>', TrafficHistory.as_view(), name='traffic'),
-    path('speed/<path:url>', SpeedApi.as_view(), name='speed'),
-    path('count/<path:url>', LinkCountApi.as_view(), name='count'),
-    path('alltraffic/', AllTrafficList.as_view(), name='all'),
-    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
-    path('documentation/', doc_json, name='documentation'),
-    path('configure/', ConfigureDetailsApi.as_view(), name='configuration'),
-    path('register/', CreateUserApi.as_view(), name='register'),
+
 ]
 
 # Q3rj7tG54k7EWUjZKt3Yg5lcso1jobNw7ALYRTcO
